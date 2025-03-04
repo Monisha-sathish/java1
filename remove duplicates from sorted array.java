75015d7573776dbb7054720fa0ee140611e301e2
Monisha-sathish <144097993+Monisha-sathish@users.noreply.github.com>
2025-03-04 18:53:41 +0530

    Add files via upload

---
+++ remove duplicates from sorted array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int j = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;//Given a sorted array, this method removes duplicates in-place, meaning it modifies the array itself without using extra space.
+    }
+}
